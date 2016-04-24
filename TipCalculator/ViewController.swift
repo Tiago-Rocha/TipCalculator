346bc05a436461b836280778027c37f6ea8e97ec
Tiago-Rocha <tiago.aziz@gmail.com>
2016-04-24 22:09:45 +0100

    tipCalculor base

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,15 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var totalTextField : UITextField!
+    @IBOutlet var taxPctSlider : UISlider!
+    @IBOutlet var taxPctLabel : UILabel!
+    @IBOutlet var resultsTextView : UITextView!
+    let tipCalc = TipCalculatorModel(total: 33.25, taxPct: 0.06)
+    
+    func refreshUI() {
+        
+        totalTextField.resignFirstResponder()
+        // 1
+        totalTextField.text = String(format: "%0.2f", tipCalc.total)
+        // 2
+        taxPctSlider.value = Float(tipCalc.taxPct) * 100.0
+        // 3
+        taxPctLabel.text = "Tax Percentage (\(Int(taxPctSlider.value))%)"
+        // 4
+        resultsTextView.text = ""
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        refreshUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func calculateTapped(sender : AnyObject) {
+        tipCalc.total = Double((totalTextField.text! as NSString).doubleValue)
+        let possibleTips = tipCalc.returnPossibleTips()
+        var results = ""
+        var keys = Array(possibleTips.keys)
+        keys.sortInPlace()
+        for tipPct in keys {
+            let tipValue = possibleTips[tipPct]!
+            let prettyTipValue = String(format:"%.2f", tipValue)
+            results += "\(tipPct)%: \(prettyTipValue)\n"
+        }
+        resultsTextView.text = results
+    }
+    
+    @IBAction func taxPercentageChanged(sender : AnyObject) {
+        tipCalc.taxPct = Double(taxPctSlider.value) / 100.0
+        NSLog("refresh UI")
+        refreshUI()
+    }
+    
+    @IBAction func viewTapped(sender : AnyObject) {
+        //dismiss keyboard
+        totalTextField.resignFirstResponder()
     }
 
 
